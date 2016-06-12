9ccfd05f4e7f7775b6dd6e69bbe9e948895fcf38
vishnu <vishnu@cumulations.com>
2016-06-12 11:40:52 +0530

    test

--- GitUser/ViewController.m
+++ GitUser/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"test account Git");
+    NSLog(@"test your account Git");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
